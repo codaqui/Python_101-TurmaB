211bf4c3cdc83ab086c34b6820d61e52cf70c2f4
anadevti <anacarolayne777@gmail.com>
2025-08-19 20:12:39 -0300

    adicionando conteudo da aula 03 + exemplos

---
+++ Modulo 01/Aula 03 - Estruturas de Repetição/aula_03_aovivo.py
@@ -0,0 +1,15 @@
+﻿import random # Biblioteca
+
+numero_secreto = random.randint(1, 10)
+tentativas = 0
+
+while True:
+    chute = int(input("Digite um numero entre 1 e 10: "))
+    tentativas += 1
+    if chute == numero_secreto:
+        print("Parabens, voce acertou em", tentativas, "tentativas!")
+        break
+    elif chute < numero_secreto:
+        print("Muito Baixo, tente de novo!")
+    else:
+        print("Muito alto, tente de novo!")
